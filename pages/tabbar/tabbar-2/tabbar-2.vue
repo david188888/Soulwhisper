26dab9450689bd51590063dd9a54ee49770539c1
Afightingcoder <20223803040@m.scun.edu.cn>
2025-03-15 23:57:38 +0800

    init view pages

---
+++ pages/tabbar/tabbar-2/tabbar-2.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="content">
+		情绪分析图表页
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		background-color: #f8fafc;
+		height: 765px;
+	}
+</style>
---
+++ pages/tabbar/tabbar-5/tabbar-5.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="content">
+		我的
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		background-color: #f8fafc;
+		height: 765px;
+	}
+</style>
