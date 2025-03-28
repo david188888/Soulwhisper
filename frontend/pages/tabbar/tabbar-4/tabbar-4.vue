63fdb0178aaf2256073c5c0118c75c1ef4b10d33
Ray77250 <20223802070@m.scnu.edu.cn>
2025-03-28 18:33:05 +0800

    psge4 navbar&tab

--- frontend/pages/tabbar/tabbar-4/tabbar-4.vue
+++ frontend/pages/tabbar/tabbar-4/tabbar-4.vue
@@ -1,11 +1,23 @@
 <template>
 	<view class="content">
-		情绪漂流瓶
+		<!--搜索栏-->
+		<navbar></navbar>
+		<!--选项卡-->
+		<tab></tab>
+		<!-- <view v-for="item in 50">
+			{{item}}hahahahahaha
+		</view> -->
 	</view>
 </template>
 
 <script>
+	import navbar from '@/frontend/components/navbar/navbar.vue';
+	import tab from '@/frontend/components/tab/tab.vue'
 	export default {
+		components:{
+			navbar,
+			tab
+		},
 		data() {
 			return {
 				title: 'Hello'
@@ -20,10 +32,5 @@
 	}
 </script>
 
-<style>
-	.content {
-		text-align: center;
-		background-color: #f8fafc;
-		height: 765px;
-	}
+<style lang="scss">
 </style>
