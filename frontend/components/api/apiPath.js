5c6c11673c0552ac1d83848e1d6aaf4e73021111
Afightingcoder <20223803040@m.scun.edu.cn>
2025-06-08 14:31:15 +0800

    feat: update user_id of uniCloud function & request baseUrl

--- frontend/components/api/apiPath.js
+++ frontend/components/api/apiPath.js
@@ -1,5 +1,4 @@
-const baseUrl = 'http://127.0.0.1:8000/';
-const diaryListUrl = 'http://127.0.0.1:8000/';
+const baseUrl = 'http://43.133.190.180:8000/';
 
 export const api = {
     register: `${baseUrl}api/account/register/`,
--- uniCloud-aliyun/cloudfunctions/update_data/index.js
+++ uniCloud-aliyun/cloudfunctions/update_data/index.js
@@ -2,7 +2,7 @@
 const db = uniCloud.database()
 exports.main = async (event, context) => {
 	const collection = db.collection('user')
-	const res = await collection.doc('682d74b08a5c782a2b825fdd').update({
+	const res = await collection.doc('6843f546fe975fd64c494da7').update({
 		author_likes_ids:["682d74b08a5c782a2b825fde"]
 	})
 
